ba10531f42a080be60898ece7b7125c00853a767
rajatchaudhary91 <wipro_rajat.chaudhary@outlook.com>
2016-02-17 19:31:07 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require("mongodb").MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  
+  mongo.connect(connectionString ,function(err,db) { 
+ if(err)    { 
+     //res.send("can not connect to db : "+err); 
+     res.render('index', { title: ''+err });
+ } 
+ else 
+ { 
+    dbObj = {db:db, 
+    employees : db.collection('Users')}; 
+     //res.send("Connection with db successful"); 
+     res.render('index', { title: 'Connection Successful' });
+   } 
+}); 
+  
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+
+/*
+var mongo = require("mongodb").MongoClient; 
+2 var dbObj; 
+3 var express = require('express'); 
+4 var employeeRouter = express.Router(); 
+5 var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI; 
+6 
+ 
+7 mongo.connect(connectionString ,function(err,db) { 
+8 if(err)    { 
+9     res.send("can not connect to db : "+err); 
+10     
+11 } 
+12 else 
+13 { 
+14     dbObj = {db:db, 
+15     employees : db.collection('Users')}; 
+16      res.send("Connection with db successful"); 
+17     } 
+18 }) 
+
+
+
+
+*/
